b224a3de82abffc5eb4b72387b2c14593ffd1947
cheretbe <a.a.orlov@gmail.com>
2019-08-09 16:01:32 +0200

    Add template import helper script

---
+++ tools/import_templates.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import zabbix_api
+import json
+
+# import_rules = "rules": {
+#     "applications": {
+#         "createMissing": True,
+#         "deleteMissing": False
+#     },
+#     "valueMaps": {
+#         "createMissing": True,
+#         "updateExisting": False
+#     },
+#     "hosts": {
+#         "createMissing": True,
+#         "updateExisting": True
+#     },
+#     "items": {
+#         "createMissing": True,
+#         "updateExisting": True,
+#         "deleteMissing": True
+#     }
+# }
+# print(import_rules)
+
+
+file_name = "/zabbix-templates/custom_templates.json"
+print("Importing templates from '{}'".format(file_name))
+
+with open("/zabbix-templates/custom_templates.json", "r") as f:
+    json_text = f.read()
+
+
+z_api = zabbix_api.ZabbixAPI(server="http://localhost/zabbix")
+z_api.login("Admin", "zabbix")
+
+
+z_api.configuration.import_(
+    {"format": "json",
+     "source": json_text,
+     "rules": {
+        "applications": {
+            "createMissing": True,
+            "deleteMissing": False
+        },
+        "templates": {
+            "createMissing": True,
+            "updateExisting": True
+        },
+        "valueMaps": {
+            "createMissing": True,
+            "updateExisting": False
+        },
+        "hosts": {
+            "createMissing": True,
+            "updateExisting": True
+        },
+        "items": {
+            "createMissing": True,
+            "updateExisting": True,
+            "deleteMissing": True
+        }
+    }
+})
+
+# print(json_text)
+
+
+# for template in z_api.template.get({"search":{"name": "custom_template__"}, "output": ["templateid", "name"]}):
+#     print(" ", template["name"])
+#     template_ids += [template["templateid"]]
+
+
+z_api.logout()
